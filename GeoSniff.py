faed88a74fb5227467dfc6c7a09b71abe6d0030d
Grant Bourque <grant@anedot.com>
2016-04-13 22:38:15 -0500

    Integrate listener and the UI
    - Modify Listener class to inherit from Thread and integrate with UI
    - Expand and add functionality to UI
    - Add automatic pruning of old data
    - Add periodic drawing of graphs to avoid slowdowns and crashes
    - Extract database and the main window classes into separate modules
    - Rename modules to follow convention
    - Remove hard-coded examples from UI
    - Fix, refactor, and reformat code along the way

--- GeoSniff.py
+++ GeoSniff.py
@@ -1,195 +1,65 @@
 #!/usr/bin/env python
 
 # This is the main script to run the project GeoSniff
+# Authors: CSC 3380 Group 20
 
+import wx
+from main_window import MainWindow
+from listener import Listener
+from database import Database
+from wx.lib.pubsub import pub
+import collections
 
-try:
-    from pyslip.tkinter_error import tkinter_error
-except ImportError:
-    print('*' * 60 + '\nSorry, you must install pySlip first\n' + '*' * 60)
-    raise
-try:
-    import wx
-except ImportError:
-    msg = 'Sorry, you must install wxPython'
-    tkinter_error(msg)
-
-import IP2Location
-import pyslip
-import pyslip.stmtr_tiles as tiles
-
-import Graph
-
-
-######
-# Various demo constants - can be removed later
-######
-
-# initial view level and position
-InitViewLevel = 5
-
-InitViewPosition = (-91.179, 30.413)
-
-
-######
-# Various GUI layout constants
-######
-
-###############################################################################
-# The main application frame
-###############################################################################
 
 class App(wx.App):
-    """wxPython app class that runs the GUI."""
+    """wxPython app class that runs the GUI and interacts with the listener and database."""
 
     def OnInit(self):
         self.frame = MainWindow(parent=None, title='GeoSniff')
         self.frame.Maximize()
         self.frame.Show()
         self.SetTopWindow(self.frame)
+        self.database = Database()
+        self.points = collections.OrderedDict()
+        pub.subscribe(self.__onClearData, 'ClearData')
         return True
 
-    def add_point_to_map(self, longitude, latitude):
-        # For deletion:
-        # self.point_layer = self.pyslip.AddPointLayer(point_data, colour=point_data_colour, radius=5)
-        # self.pyslip.DeleteLayer(self.point_layer)
-        # self.point_layer = None
+    def is_sniffing(self):
+        return self.frame.sniffing
 
-        point_data = [(longitude, latitude)]
-        point_data_colour = '#ff000080'  # semi-transparent
-        self.frame.pyslip.AddPointLayer(point_data, colour=point_data_colour, radius=5)
+    def add_point_to_map(self, longitude, latitude, point_data_colour='#ff000080'):
+        return self.frame.pyslip.AddPointLayer([(longitude, latitude)], colour=point_data_colour, radius=5)
 
+    def receive(self, ip):
+        if len(self.points) > 9:
+            old_point = self.points.popitem(False)
+            self.frame.pyslip.DeleteLayer(old_point[1])
+            old_location = self.database.get_location(old_point[0])
+            self.frame.graphs_panel.prune_graphs(old_location[2], old_location[3], old_point[0])
+        location = self.database.get_location(ip)
+        if ip not in self.points:
+            self.points[ip] = self.add_point_to_map(location[1], location[0])
+        self.frame.graphs_panel.update_graphs(location[2], location[3], ip)
 
+    def set_my_ip(self, my_ip):
+        location = self.database.get_location(my_ip)
+        self.add_point_to_map(location[1], location[0], '#0000ff')
+        self.frame.my_ip_view.SetLabel("Your IP address: " + my_ip)
 
-
-
-class MainWindow(wx.Frame):
-    """Window frame to contain the panels for functionality."""
-
-    def __init__(self, parent, title):
-        wx.Frame.__init__(self, parent, title=title)
-
-        self.actionbar_panel = wx.Panel(self)
-        temp_label = wx.StaticText(self.actionbar_panel, -1, "Buttons will go here")
-
-        self.split_graph_map = wx.SplitterWindow(self, style=wx.SP_LIVE_UPDATE)
-
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(self.actionbar_panel, 0, wx.ALL, 5)
-        sizer.Add(self.split_graph_map, 1, wx.EXPAND | wx.ALL, 5)
-
-        self.SetSizer(sizer)
-
-        # MAKE GRAPHS PANEL
-        self.graphs_panel = Graph.GraphPanelHolder(self.split_graph_map)
-
-        # FOR GRAPH TESTING PURPOSES
-        data = Database()
-        loc = data.get_location("167.96.56.212")
-        self.graphs_panel.update_graphs(loc[2], loc[3], "167.96.52.212")
-        loc= data.get_location("8.8.8.8")
-        self.graphs_panel.update_graphs(loc[2], loc[3], "167.96.52.222")
-        loc= data.get_location("210.129.120.46")
-        self.graphs_panel.update_graphs(loc[2], loc[3], "210.129.120.46")
-        # END GRAPH STUFF
-
-        self.split_graph_map.Initialize(self.graphs_panel)
-        self.map_panel = wx.Panel(self.split_graph_map)
-        self.map_panel.ClearBackground()
-
-        self.tile_source = tiles.Tiles()
-
-        # build the GUI
-        self.make_gui(self.map_panel)
-
-        # do initialisation stuff - all the application stuff
-        self.init()
-
-        self.split_graph_map.SplitVertically(self.graphs_panel, self.map_panel)
-        self.split_graph_map.SetMinimumPaneSize(300)
-
-
-    #####
-    # Build the GUI
-    #####
-
-    def make_gui(self, parent):
-        """Create application GUI."""
-
-        # start application layout
-        all_display = wx.BoxSizer(wx.HORIZONTAL)
-        parent.SetSizer(all_display)
-
-        # put map view in left of horizontal box
-        sl_box = self.make_gui_view(parent)
-        all_display.Add(sl_box, proportion=1, flag=wx.EXPAND)
-
-        parent.SetSizerAndFit(all_display)
-
-    def make_gui_view(self, parent):
-        """Build the map view widget
-
-        parent  reference to the widget parent
-
-        Returns the static box sizer.
-        """
-
-        # create gui objects
-        sb = wx.StaticBox(parent)
-        self.pyslip = pyslip.PySlip(parent, tile_src=self.tile_source)
-
-        # lay out objects
-        box = wx.StaticBoxSizer(sb, orient=wx.HORIZONTAL)
-        box.Add(self.pyslip, proportion=1, flag=wx.EXPAND)
-
-        return box
-
-    ######
-    # Finish initialization of data, etc
-    ######
-
-    def init(self):
-        # force pyslip initialisation
-        self.pyslip.OnSize()
-
-        # set initial view position
-        wx.CallAfter(self.final_setup, InitViewLevel, InitViewPosition)
-
-    def final_setup(self, level, position):
-        """Perform final setup.
-
-        level     zoom level required
-        position  position to be in centre of view
-
-        We do this in a CallAfter() function for those operations that
-        must not be done while the GUI is "fluid".
-        """
-
-        self.pyslip.GotoLevelAndPosition(level, position)
+    def __onClearData(self):
+        for point_layer in self.points.values():
+            self.frame.pyslip.DeleteLayer(point_layer)
+        self.points.clear()
 
 
 ###############################################################################
 
-# Class for getting data from the database
-class Database:
-    # Creates a object with IP2Location API
-    def __init__(self):
-        self.database = IP2Location.IP2Location("IP2LOCATION-LITE-DB11.BIN")
-
-    # Return a list that has Latitude = 0, Longitude = 1, Country = 2, city = 3
-    def get_location(self, ip):
-        sender = self.database.get_all(ip)
-        ret = [sender.latitude, sender.longitude, sender.country_long, sender.region]
-        return ret
-
-
 if __name__ == '__main__':
-    # start wxPython app
+    # Start wxPython app
     app = App()
 
-    data = Database()
-    loc = data.get_location("167.96.56.212")
-
-    app.add_point_to_map(loc[1], loc[0])
+    # Start listener thread
+    listener = Listener(app)
+    listener.start()
 
     app.MainLoop()
--- graph.py
+++ graph.py
@@ -1,15 +1,17 @@
-# Author: Vu Le
-
+import threading
+import time
 import wx
+from wx.lib.pubsub import pub
 import matplotlib
 
 matplotlib.use('WXAgg')
 from matplotlib.figure import Figure
-from matplotlib.backends.backend_wxagg import \
-    FigureCanvasWxAgg as FigCanva
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg
 
-# STATE LOOKUP
-us_state_abbrev = {
+# Authors: Vu Le, Grant Bourque
+
+# STATE ABBREVIATION LOOKUP
+us_state_abbrs = {
     'Alabama': 'AL',
     'Alaska': 'AK',
     'Arizona': 'AZ',
@@ -62,19 +64,29 @@ us_state_abbrev = {
     'Wyoming': 'WY',
 }
 
-# Panel to hold graphs
+
+class DrawThread(threading.Thread):
+    """Thread class for updating the graphs approximately every 1 second"""
+
+    def __init__(self, panel):
+        threading.Thread.__init__(self)
+        self.daemon = True
+        self.panel = panel
+
+    def run(self):
+        while True:
+            time.sleep(1)
+            wx.CallAfter(self.panel.draw_graphs)
+
+
 class GraphPanelHolder(wx.Panel):
-    countries = []
-    country_data = []
-
-    states = []
-    state_data = []
-
-    ips = []
-    ip_data = []
+    """Panel for holding graphs"""
 
     def __init__(self, parent):
         wx.Panel.__init__(self, parent)
+        self.countries = dict()
+        self.states = dict()
+        self.ips = dict()
         sizer = wx.BoxSizer(wx.VERTICAL)
         self.country_graph = GraphPanel(self, graph_type="country")
         self.state_graph = GraphPanel(self, graph_type="state")
@@ -84,50 +96,75 @@ class GraphPanelHolder(wx.Panel):
         sizer.Add(self.ip_graph, 1, wx.EXPAND)
 
         self.SetSizer(sizer)
+        pub.subscribe(self.__onClearData, 'ClearData')
+
+        draw_thread = DrawThread(self)
+        draw_thread.start()
 
     def update_graphs(self, country, state, ip):
-        self.update_countries(country)
-        self.update_states(state)
-        self.update_ips(ip)
+        self.increase_country(country)
+        self.increase_state(state)
+        self.increase_ip(ip)
+
+    def prune_graphs(self, country, state, ip):
+        self.decrease_country(country, ip)
+        self.decrease_state(state, ip)
+        self.delete_ip(ip)
+
+    def draw_graphs(self):
         self.country_graph.draw()
         self.state_graph.draw()
         self.ip_graph.draw()
 
-    def update_countries(self, country):
-        for i in range(len(self.countries)):
-            if country == self.countries[i]:
-                self.country_data[i] = self.country_data[i] + 1
-                return
-        self.countries.append(country)
-        self.country_data.append(1)
-
-    def update_states(self, state):
-        if state in us_state_abbrev:
-            state_abbr = us_state_abbrev.get(state)
+    def increase_country(self, country):
+        if country in self.countries:
+            self.countries[country] += 1
         else:
-            state_abbr = "OTHER"
-        for i in range(len(self.states)):
-            if state_abbr == self.states[i]:
-                self.state_data = self.state_data[i] + 1
-                return
-        self.states.append(state_abbr)
-        self.state_data.append(1)
+            self.countries[country] = 1
 
-    def update_ips(self, ip):
-        for i in range(len(self.ips)):
-            if ip == self.ips[i]:
-                ip_data = self.ip_data[i] + 1
-                return
-        self.ips.append(ip)
-        self.ip_data.append(1)
+    def decrease_country(self, country, ip):
+        self.countries[country] -= self.ips[ip]
+        if self.countries[country] == 0:
+            del self.countries[country]
+
+    def increase_state(self, state):
+        state_abbr = us_state_abbrs.get(state, "OTHER")
+        if state_abbr in self.states:
+            self.states[state_abbr] += 1
+        else:
+            self.states[state_abbr] = 1
+
+    def decrease_state(self, state, ip):
+        state_abbr = us_state_abbrs.get(state, "OTHER")
+        self.states[state_abbr] -= self.ips[ip]
+        if self.states[state_abbr] == 0:
+            del self.states[state_abbr]
+
+    def increase_ip(self, ip):
+        if ip in self.ips:
+            self.ips[ip] += 1
+        else:
+            self.ips[ip] = 1
+
+    def delete_ip(self, ip):
+        del self.ips[ip]
+
+    def __onClearData(self):
+        self.countries.clear()
+        self.states.clear()
+        self.ips.clear()
+        self.draw_graphs()
 
 
-# Individual graph panel
 class GraphPanel(wx.Panel):
+    """Individual graph panel"""
+
     def __init__(self, parent, graph_type):
         self.graph_type = graph_type
 
         wx.Panel.__init__(self, parent)
+
+        self.holder = parent
 
         self.figure = matplotlib.figure.Figure()
         self.canvas = matplotlib.backends.backend_wxagg.FigureCanvasWxAgg(self, -1, self.figure)
@@ -157,14 +194,14 @@ class GraphPanel(wx.Panel):
     def draw(self):
         self.figure.clf()
         ax = self.figure.gca()
-        if (self.graph_type == "country"):
-            ax.pie(GraphPanelHolder.country_data,  labels=GraphPanelHolder.countries,
+        if self.graph_type == "country":
+            ax.pie(self.holder.countries.values(), labels=self.holder.countries.keys(),
                    autopct='%1.1f%%', shadow=True, startangle=90)
-        elif (self.graph_type == "state"):
-            ax.pie(GraphPanelHolder.state_data, labels=GraphPanelHolder.states,
+        elif self.graph_type == "state":
+            ax.pie(self.holder.states.values(), labels=self.holder.states.keys(),
                    autopct='%1.1f%%', shadow=True, startangle=90)
-        elif (self.graph_type == "ip"):
-            ax.pie(GraphPanelHolder.ip_data, labels=GraphPanelHolder.ips,
+        elif self.graph_type == "ip":
+            ax.pie(self.holder.ips.values(), labels=self.holder.ips.keys(),
                    autopct='%1.1f%%', shadow=True, startangle=90)
         ax.axis('equal')
         self.canvas.draw()
